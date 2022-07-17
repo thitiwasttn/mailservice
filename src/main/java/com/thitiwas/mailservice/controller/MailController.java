49f48ae65a507b7812e0975c58dc7fc45dbce30e
Thitiwas <55071973+thitiwasttn@users.noreply.github.com>
2022-07-18 00:42:53 +0700

    Update MailController.java

--- src/main/java/com/thitiwas/mailservice/controller/MailController.java
+++ src/main/java/com/thitiwas/mailservice/controller/MailController.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Calendar;
 import java.util.List;
 
